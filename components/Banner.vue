5981501db56cf689da809e98ab8a115fcd897eff
Hugo Valla <Hugo_valla@hotmail.fr>
2020-10-06 20:23:13 +0200

    Basi interface for Algolia

---
+++ components/Banner.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="banner">
+    <div class="banner--wrapper">
+      <h1>Inspiring <strong>talks </strong> from <strong>Ted</strong>!</h1>
+      <p>Filters & enjoy the best talks</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Banner",
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang="scss" scoped>
+.banner {
+  background: url("../Assets/Images/banner.svg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 80vh;
+}
+
+.banner--wrapper {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  height: 100%;
+  h1 {
+    margin-top: 16rem;
+    align-self: center;
+    color: rgb(248, 247, 247);
+    font-family: "Poppins", sans-serif;
+    font-size: 32px;
+  }
+  p {
+    align-self: center;
+    color: rgb(248, 247, 247);
+    font-family: "Poppins", sans-serif;
+  }
+}
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,102 @@
 <template>
   <div>
-    <header class="header">
-      <h1 class="header-title"><a href="/">Vue InstantSearch v2 starter</a></h1>
-      <p class="header-subtitle">
-        using
-        <a href="https://github.com/algolia/vue-instantsearch">
-          Vue InstantSearch
-        </a>
-      </p>
-    </header>
+    <Banner />
 
     <div class="container">
-      <ais-instant-search
-        :search-client="searchClient"
-        index-name="demo_ecommerce"
-      >
+      <ais-instant-search :search-client="searchClient" index-name="Teds">
+        <ais-stats>
+          <p
+            slot-scope="{
+              hitsPerPage,
+              nbPages,
+              nbHits,
+              page,
+              processingTimeMS,
+              query,
+            }"
+            class="text--italic"
+          >
+            Page {{ page + 1 }} of {{ nbPages }} with {{ hitsPerPage }} hits per
+            page - {{ nbHits }} hits retrieved in {{ processingTimeMS }}ms for
+            <q>{{ query }} </q> - Yeahh 👏
+          </p>
+        </ais-stats>
         <div class="search-panel">
           <div class="search-panel__filters">
-            <ais-refinement-list attribute="categories" searchable />
+            <ais-clear-refinements />
+            <h2>FILTERS</h2>
+            <ais-refinement-list attribute="tags" :limit="15" searchable />
+            <ais-configure :hitsPerPage="8" />
+            <p>DURATION</p>
+            <p class="text--italic text--small">
+              1 is under 10', 4 is above 30'
+            </p>
+            <ais-range-input attribute="duration_range">
+              <div slot-scope="{ currentRefinement, range, refine }">
+                <vue-slider
+                  :min="range.min"
+                  :max="range.max"
+                  :lazy="true"
+                  :value="toValue(currentRefinement, range)"
+                  @change="refine({ min: $event[0], max: $event[1] })"
+                />
+              </div>
+            </ais-range-input>
+            <p>POPULARITY</p>
+            <p class="text--italic text--small">
+              Number of people who vote for this speech
+            </p>
+            <ais-range-input attribute="popularity_score">
+              <div slot-scope="{ currentRefinement, range, refine }">
+                <vue-slider
+                  :min="range.min"
+                  :max="range.max"
+                  :lazy="true"
+                  :value="toValue(currentRefinement, range)"
+                  @change="refine({ min: $event[0], max: $event[1] })"
+                />
+              </div>
+            </ais-range-input>
+            <p>LANGUAGES</p>
+            <p class="text--italic text--small">
+              Subtitles available in more than 100 languages
+            </p>
+            <ais-menu-select attribute="languages" :limit="200" />
           </div>
 
           <div class="search-panel__results">
-            <ais-search-box placeholder="Search here…" class="searchbox" />
-            <ais-hits>
-              <template slot="item" slot-scope="{ item }">
+            <ais-search-box
+              placeholder="Search for a speech..."
+              class="searchbox"
+            />
+            <ais-infinite-hits :show-previous="true">
+              <!-- <ais-configure :hits-per-page.camel="14" /> -->
+              <button
+                slot="loadPrevious"
+                slot-scope="{ page, isFirstPage, refinePrevious }"
+                :disabled="isFirstPage"
+                @click="refinePrevious"
+              >
+                Show previous results (page: {{ page + 1 }})
+              </button>
+
+              <template slot="item" slot-scope="{ item }" class="mainDom">
                 <h1><ais-highlight :hit="item" attribute="name" /></h1>
-                <p><ais-highlight :hit="item" attribute="description" /></p>
+                <img
+                  :src="item.image_url"
+                  :hit="item"
+                  attribute="image_url"
+                  class="mainDom--img"
+                />
+                <div class="hit-description">
+                  <p>{{ item.description }}</p>
+                  <p class="speakers">{{ item.speakers }}</p>
+                  <p class="views">
+                    <i class="fab fa-youtube"></i> {{ item.viewed_count }}
+                  </p>
+                </div>
               </template>
-            </ais-hits>
+            </ais-infinite-hits>
 
             <div class="pagination"><ais-pagination /></div>
           </div>
@@ -38,67 +107,200 @@
 </template>
 
 <script>
-import algoliasearch from 'algoliasearch/lite';
-import 'instantsearch.css/themes/algolia-min.css';
+import algoliasearch from "algoliasearch/lite";
+import "instantsearch.css/themes/algolia-min.css";
+import VueSlider from "vue-slider-component";
+import "vue-slider-component/theme/default.css";
+import Banner from "../components/Banner";
 
 export default {
+  components: {
+    VueSlider,
+    Banner,
+  },
   data() {
     return {
       searchClient: algoliasearch(
-        'B1G2GM9NG0',
-        'aadef574be1f9252bb48d4ea09b5cfe5'
+        "J4CSRPH96F",
+        "ff7239d8bd49e1b9000920a803df4132"
       ),
     };
+  },
+  methods: {
+    toValue(value, range) {
+      return [
+        value.min !== null ? value.min : range.min,
+        value.max !== null ? value.max : range.max,
+      ];
+    },
   },
 };
 </script>
 
-<style>
+<style lang="scss">
+//GLOBAL STYLES
+
 body,
 h1 {
   margin: 0;
   padding: 0;
 }
 
+h1,
+h2,
+h3,
+p {
+  color: #454141;
+}
+
+.text--italic {
+  font-style: italic;
+}
+.text--small {
+  font-size: 10px;
+}
+
 body {
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica,
-    Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
-}
-
-.header {
-  display: flex;
-  align-items: center;
-  min-height: 50px;
-  padding: 0.5rem 1rem;
-  background-image: linear-gradient(to right, #4dba87, #2f9088);
-  color: #fff;
-  margin-bottom: 1rem;
-}
-
-.header a {
-  color: #fff;
-  text-decoration: none;
-}
-
-.header-title {
-  font-size: 1.2rem;
-  font-weight: normal;
-}
-
-.header-title::after {
-  content: ' ▸ ';
-  padding: 0 0.5rem;
-}
-
-.header-subtitle {
-  font-size: 1.2rem;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica,
+    Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  background-color: white;
 }
 
 .container {
-  max-width: 1200px;
-  margin: 0 auto;
-  padding: 1rem;
+  margin: 2rem 1rem 2rem 1rem;
+  padding: 2rem;
+  box-shadow: 5px 5px 20px rgba(0, 0, 0, 0.5);
+  border-radius: 30px;
+  background-color: rgb(255, 253, 253);
 }
+
+// RIGHT PANEL
+
+.ais-Stats p {
+  text-align: right;
+  font-size: 12px;
+  color: #454141;
+}
+
+.ais-SearchBox-input {
+  border-radius: 30px;
+}
+
+.ais-ClearRefinements-button,
+.ais-InfiniteHits-loadMore {
+  border-radius: 30px;
+  padding: 10px 20px 10px 20px;
+  background-color: #f90f00;
+  cursor: pointer;
+  transition: all 0.5s ease-in-out;
+
+  &:hover {
+    background-color: #454141;
+    color: rgb(248, 247, 247);
+    transition: all 0.5s ease-in-out;
+  }
+}
+
+.ais-InfiniteHits-loadMore {
+  transform: translateX(25vw);
+  margin: 2rem auto 2rem auto;
+  background-color: #f90f00;
+}
+
+.ais-InfiniteHits-list {
+  display: grid;
+  grid-gap: 1rem;
+  grid-template-columns: repeat(3, 30%);
+  margin: 0 auto;
+  justify-content: center;
+
+  @media screen and (max-width: 992px) {
+    grid-template-columns: repeat(2, 45%);
+  }
+  @media screen and (max-width: 772px) {
+    grid-template-columns: repeat(1, 80%);
+  }
+}
+.mainDom--img {
+  width: 50%;
+  margin: 0 auto;
+  border-radius: 10px;
+  justify-self: center;
+  margin-bottom: 1rem;
+  filter: contrast(120%);
+  transition: all 0.3s ease-in-out;
+}
+
+.ais-InfiniteHits-item {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  border: none;
+  box-shadow: 5px 5px 15px rgba(249, 15, 0, 0.1);
+  border-radius: 30px;
+  transition: all 0.2s ease-in-out;
+  cursor: pointer;
+
+  h1 {
+    text-align: center;
+    margin-bottom: 1rem;
+    color: #454141;
+    font-size: 16px;
+  }
+
+  &:hover {
+    transition: all 0.3s ease-in-out;
+    transform: scale(1.1);
+
+    img {
+      filter: contrast(180%);
+      transition: all 0.3s ease-in-out;
+    }
+  }
+}
+
+.hit-description {
+  p {
+    font-size: 12px;
+    text-align: center;
+  }
+
+  .speakers {
+    font-size: 10px;
+    text-align: right;
+    color: #f90f00;
+    font-style: italic;
+    font-weight: bold;
+  }
+
+  .views {
+    margin-top: 2rem;
+    text-align: right;
+    .fa-youtube {
+      color: #f90f00;
+    }
+  }
+}
+
+//PAGINATION
+
+.ais-Pagination-item,
+.ais-Pagination-link {
+  border: none !important;
+  background-color: transparent !important;
+  color: #454141;
+  font-family: "Poppins", serif !important;
+}
+
+.ais-Pagination-item--selected {
+  border: #f90f00 !important;
+  background-color: #f90f00 !important;
+  border-radius: 30px;
+  color: white !important;
+  font-family: "Poppins", serif !important;
+}
+
+//LEFT PANEL
 
 .search-panel {
   display: flex;
@@ -107,6 +309,25 @@ body {
 .search-panel__filters {
   flex: 1;
   margin-right: 1em;
+  box-shadow: 5px 5px 15px rgba(249, 15, 0, 0.1);
+  border-radius: 30px;
+  padding: 1rem;
+}
+
+.ais-RefinementList-list {
+  margin: 1rem auto 4rem;
+}
+
+.vue-slider-ltr {
+  margin: 1rem auto 4rem;
+}
+
+.ais-RefinementList-searchBox {
+  display: none;
+}
+
+.ais-MenuSelect-select {
+  border-radius: 30px;
 }
 
 .search-panel__results {
